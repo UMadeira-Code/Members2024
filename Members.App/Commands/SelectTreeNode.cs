c392b8960fabc5b45e4e20a4735909f648251b80
Leonel Nobrega <lnobrega@uma.pt>
2024-04-19 13:58:37 +0100

    SelectedNode Command

---
+++ Members.App/Commands/SelectTreeNode.cs
@@ -0,0 +1,27 @@
+﻿using Members.Core.Commands;
+
+namespace Members.App.Commands
+{
+    public class SelectTreeNode : ICommand
+    {
+        public SelectTreeNode( TreeNode node )
+        {
+            Node = node;
+            View = node.TreeView;
+        }
+
+        private TreeView View { get; set; }
+        private TreeNode Node { get; set; }
+
+        public void Do()
+        {
+            var node = View.SelectedNode;
+            View.SelectedNode = Node;
+            Node = node;
+        }
+
+        public void Undo() => Do();
+        public void Redo() => Do();
+        public void Cancel() { }
+    }
+}
