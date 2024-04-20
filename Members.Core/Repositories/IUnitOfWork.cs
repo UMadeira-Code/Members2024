2ffe2cbc22323a367f72c2a7234b0f1663529c9f
Leonel Nobrega <lnobrega@uma.pt>
2024-04-20 14:48:26 +0100

    Rmove Factory from UnitOfWork

--- Members.Core/Repositories/IUnitOfWork.cs
+++ Members.Core/Repositories/IUnitOfWork.cs
@@ -4,8 +4,6 @@ namespace Members.Core.Repositories
 {
     public interface IUnitOfWork : IDisposable
     {
-        IFactory Factory { get; }
-
         IRepository<TEntity>? GetRepository<TEntity>() where TEntity : Item;
 
         void Begin();
